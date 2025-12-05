c10a157d7071cdedf4bfc74815bed4b3509f8103
Robinson <robinsonmunoz.abreu@gmail.com>
2025-12-05 13:07:44 -0300

    Delete backend directory

--- backend/src/routes/excel.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { exportExcelOT } from "../controllers/excel.controller.js";
-
-const router = Router();
-
-router.get("/ot/:id/export", exportExcelOT);
-
-export default router;
